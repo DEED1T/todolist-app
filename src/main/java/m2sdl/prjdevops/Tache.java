9ea69e7f014a0f97b8b487743491861ed8109376
notdelirious <t1-du-31@hotmail.fr>
2024-05-23 09:04:45 +0200

    feat: Added Tache bean
    Setup a constructor that takes a boolean and a description relative to task.

---
+++ src/main/java/m2sdl/prjdevops/Tache.java
@@ -0,0 +1,27 @@
+package m2sdl.prjdevops;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Tache {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private boolean done;
+    private String description;
+
+    public Tache(Boolean done, String description) {
+        this.done = done;
+        this.description = description;
+    }
+}
