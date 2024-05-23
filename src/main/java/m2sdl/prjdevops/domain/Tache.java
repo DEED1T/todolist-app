6433a17f9f8d7fc7a4281b580e8aa19d7d6be5e1
notdelirious <t1-du-31@hotmail.fr>
2024-05-23 10:58:40 +0200

    feat: Added Tache validation constraints.
    boolean done must not be null and the description string must not be null or and/or empty.

--- src/main/java/m2sdl/prjdevops/domain/Tache.java
+++ src/main/java/m2sdl/prjdevops/domain/Tache.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,7 +19,12 @@ public class Tache {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull
     private boolean done;
+
+    @NotEmpty
+    @NotNull
     private String description;
 
     public Tache(Boolean done, String description) {
