0336d8dc11e3101b280afaa62b3648a0c1bef78c
notdelirious <t1-du-31@hotmail.fr>
2024-05-29 21:30:10 +0200

    feat(test): Added tests and a dadaloader.

---
+++ src/test/java/m2sdl/prjdevops/TacheRepositoryTest.java
@@ -0,0 +1,4 @@
+package m2sdl.prjdevops;
+
+public class TacheRepositoryTest {
+}
---
+++ src/test/java/m2sdl/prjdevops/TacheServiceTest.java
@@ -0,0 +1,7 @@
+package m2sdl.prjdevops;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TacheServiceTest {
+}
