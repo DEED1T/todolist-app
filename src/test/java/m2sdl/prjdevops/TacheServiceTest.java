978dbc66121eeaaf5ab2e6baa298490dda4a735b
notdelirious <t1-du-31@hotmail.fr>
2024-05-30 21:08:40 +0200

    Forgot to push tache service unit tests.

--- src/test/java/m2sdl/prjdevops/TacheServiceTest.java
+++ src/test/java/m2sdl/prjdevops/TacheServiceTest.java
@@ -1,7 +1,48 @@
 package m2sdl.prjdevops;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import jakarta.persistence.EntityNotFoundException;
+import m2sdl.prjdevops.repository.TacheRepository;
+import m2sdl.prjdevops.service.TacheService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-public class TacheServiceTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class TacheServiceTest {
+
+    private TacheService tacheService;
+
+    @MockBean
+    private TacheRepository tacheRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.tacheService = new TacheService(tacheRepository);
+    }
+
+    @Test
+    void testRepositoryType() {
+        assertThat(tacheService.getTacheRepository(), instanceOf(CrudRepository.class));
+    }
+
+    @Test
+    void testFindByIdFromCrudRepositoryIsInvokedWhenTacheIsFoundById() {
+        assertThrows(EntityNotFoundException.class, () -> tacheService.findTacheById(0L));
+        verify(tacheService.getTacheRepository()).findById(0L);
+    }
+
+    @Test
+    void testFindAllFromCrudRepositoryIsInvokedWhenFindAllTaches() {
+        tacheService.findAllTaches();
+        verify(tacheService.getTacheRepository()).findAll();
+    }
+
 }
